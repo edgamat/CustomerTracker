58bb283e8679d331805b6eee2161e585bc6136f1
Matthew Edgar <edgarm@infinity-software.com>
2020-02-16 05:34:00 -0500

    first working version

---
+++ src/CustomerTracker.Persistence/CustomerConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CustomerTracker.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace CustomerTracker.Persistence
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration
+    {
+        public void Configure(ModelBuilder modelBuilder)
+        {
+            if (modelBuilder == null) throw new ArgumentNullException(nameof(modelBuilder));
+
+            modelBuilder.Entity<Customer>(builder =>
+            {
+                builder.Property(e => e.Id).HasValueGenerator<SequentialGuidValueGenerator>();
+                builder.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                builder.Property(e => e.EmailAddress).HasMaxLength(255);
+                builder.Property(e => e.IsActive).HasDefaultValue(true);
+            });
+        }
+    }
+}
