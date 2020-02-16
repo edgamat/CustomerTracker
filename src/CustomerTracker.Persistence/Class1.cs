bae439f87a5824afbfa5b9031257de053ac6ceb5
Matthew Edgar <edgarm@infinity-software.com>
2020-02-16 03:58:02 -0500

    initial commit

---
+++ src/CustomerTracker.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CustomerTracker.Persistence
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/UnitTests.CustomerTracker.Persistence/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UnitTests.CustomerTracker.Persistence
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
